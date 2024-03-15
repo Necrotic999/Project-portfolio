8c9c8b185cdc650693ae1ac6a5273d23717064f0
Lulia131313 <146219985+Lulia131313@users.noreply.github.com>
2024-03-15 17:32:56 +0200

    julia/Project

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,47 +1,58 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const images = [
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
     description: 'Hokkaido Flower',
   },
   {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
     description: 'Container Haulage Freight',
   },
 ];
+const swiperWrapper = document.querySelector('.swiper-wrapper');
 
-const imageList = document.querySelector('.swiper-wrapper');
+function imageTemplate(img) {
+  return `
+    <div class="swiper-slide">
+      <img width="311" height="195" src="${img.original}" alt="${img.description}">
+    </div>`;
+}
 
-images.forEach(image => {
-  const listItem = document.createElement('div');
-  listItem.classList.add('swiper-slide');
+function imagesTemplate(array) {
+  return array.map(imageTemplate).join('');
+}
 
-  const img = document.createElement('img');
-  img.src = image.preview;
-  img.alt = image.description;
-  img.classList.add('gallery-image');
+function imagesCreate(array) {
+  const markup = imagesTemplate(array);
+  swiperWrapper.innerHTML = markup;
+}
 
-  listItem.appendChild(img);
-  imageList.appendChild(listItem);
-});
+imagesCreate(images);
 
-const swiper = new Swiper('.swiper-container', {
-  direction: 'horizontal',
+Swiper.use([Navigation, Pagination]);
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
+const mySwiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  slidesPerView: 'auto',
+  spaceBetween: 10,
 });
