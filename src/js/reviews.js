7c1afeb551d332486e8cfca2bf969cfc4d0960c5
NatLuts <lutsenko.natusiia@gmail.com>
2024-03-15 18:39:10 +0200

    update

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,3 +1,5 @@
+
+
 import axios from 'axios';
 
 const BASE_URL = 'https://portfolio-js.b.goit.study/api';
@@ -7,8 +9,41 @@ export async function getReviews() {
 
     const url = `${BASE_URL}${END_POINT}`
 
-    const res = await axios.get(url);
-    return res.data;
+    const {data} = await axios.get(url);
+    return data;
+  
 }
   
-getReviews().then(arr => console.log(arr))
+getReviews().then(arr => console.log(arr));
+
+
+const gallery =document.querySelector('.js-reviews-list')
+
+function createMarkup(arr){
+    return arr.map(arr => {
+const {
+    author,
+    avatar_url,
+    review
+} = arr;
+return `
+<li class="gallery-item">
+   <img class="gallery-image"
+   src = "${avatar_url}"
+   alt = "${author}"/>
+   <h4 class="review-author ">${author}</h4>
+   <p class="review-text">${review}</p>
+</li>
+`
+    }).join('');
+}
+
+async function renderPage(arr){
+    try {
+        const res = await getReviews();
+        gallery.insertAdjacentHTML('beforeend', createMarkup(res));
+    }catch (error){
+        console.log(error);
+    } 
+}
+renderPage()
