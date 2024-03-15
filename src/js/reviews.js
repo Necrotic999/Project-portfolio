a6aa81a77c39c82532d37683f29398f79eac6194
NatLuts <lutsenko.natusiia@gmail.com>
2024-03-15 11:33:39 +0200

    upd 1

---
+++ src/js/reviews.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://portfolio-js.b.goit.study/api';
+const END_POINT = "/reviews"
+
+export async function getReviews() {
+
+    const url = `${BASE_URL}${END_POINT}`
+
+    const res = await axios.get(url);
+    return res.data;
+}
+  
