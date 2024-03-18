79775967b5f3b81c0eea6ffd7c797eff3c6353ca
Necrotic999 <l.ilya041105@gmail.com>
2024-03-18 17:56:46 +0200

    update icons sprite

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -202,9 +202,8 @@ function modalTemplate(obj) {
     <div class="modal-overlay">
       <div class="modal-content">
         <span class="close-btn">
-          <svg class="close" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M18 6L6 18" stroke="#FAFAFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M6 6L18 18" stroke="#FAFAFA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <svg class="close" width="24" height="24">
+            <use href="./img/icons/icons.svg#icon-x"></use>
           </svg>
         </span>
         <h2 class="modal-title">${obj.data.title}</h2>
